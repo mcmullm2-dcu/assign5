14faddbd129d32ad8415808041a87062dcf77302
michaelmcmullin <m.mcmullin@outlook.com>
2019-04-16 09:37:52 +0100

    Adds functional Log Out menu item

--- app/src/main/java/michaelmcmullin/sda/firstday/MainActivity.java
+++ app/src/main/java/michaelmcmullin/sda/firstday/MainActivity.java
@@ -1,13 +1,79 @@
 package michaelmcmullin.sda.firstday;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
+  /**
+   * Called when {@link MainActivity} is started, initialising the Activity and inflating the
+   * appropriate XML layout.
+   *
+   * @param savedInstanceState Used if this Activity is re-initialised, where it contains the
+   *     most recently available data (or null).
+   */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
+
+  /**
+   * Specify the menu to display for this activity.
+   * @param menu The options menu to place the menu items into.
+   * @return Returns <code>true</code> if the menu is to be displayed.
+   */
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  /**
+   * Responds to a menu item being selected.
+   * @param item The menu item selected by the user
+   * @return Returns <code>true</code> if the menu item functionality is being handled here rather
+   *     than by the normal system menu processing.
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    Log.i(AppConstants.TAG, "Starting onOptionsItemSelected method");
+
+    switch (item.getItemId()) {
+      case R.id.menuitem_log_out:
+        Logout();
+        return true;
+    }
+    return false;
+  }
+
+  /**
+   * Logs a user out of the app, redirecting them back to {@link LoginActivity}.
+   */
+  public void Logout() {
+    setContentView(R.layout.activity_login);
+    AuthUI.getInstance()
+        .signOut(this)
+        .addOnCompleteListener(new OnCompleteListener<Void>() {
+          public void onComplete(@NonNull Task<Void> task) {
+            Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+            // Back to Sign-in screen
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+          }
+        });
+  }
+
 }
