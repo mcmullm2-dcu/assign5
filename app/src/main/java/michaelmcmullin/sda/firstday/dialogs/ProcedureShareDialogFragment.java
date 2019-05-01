05c0e0e907c48244898c69a0da45a0ba33e3c6f0
mcmullm2-dcu <m.mcmullin@mcgowansprint.com>
2019-05-01 12:43:41 +0100

    Adds copyright header to all files.
    Also deletes a couple of unnecessary files, like old app icons.

--- app/src/main/java/michaelmcmullin/sda/firstday/dialogs/ProcedureShareDialogFragment.java
+++ app/src/main/java/michaelmcmullin/sda/firstday/dialogs/ProcedureShareDialogFragment.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2019 Michael McMullin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package michaelmcmullin.sda.firstday.dialogs;
 
 import android.content.Intent;
@@ -51,8 +68,10 @@ public class ProcedureShareDialogFragment extends DialogFragment {
    */
   private EditText emailAddress;
 
+  /**
+   * Empty constructor required.
+   */
   public ProcedureShareDialogFragment() {
-    // Empty constructor required.
   }
 
   /**
@@ -88,8 +107,7 @@ public class ProcedureShareDialogFragment extends DialogFragment {
 
         // Create a QR code to include in the email.
         if (procedure != null) {
-          Bitmap attachment = qr.GenerateQrCode(procedure
-              .getId()); // = BitmapFactory.decodeResource(getResources(), R.drawable.qrcode);
+          Bitmap attachment = qr.GenerateQrCode(procedure.getId());
 
           // Convert the Bitmap to a URI
           Uri imageUri = saveImage(attachment);
